f340db50cddf2f97a53b869ae09efc1ab7ee86aa
Boris W. Waguia <boris.waguia@adorsys.com>
2012-06-02 09:22:05 +0100

    Add Copyright.

--- src/test/java/org/adorsys/waguia/lightxls/test/TestEasyXlsLoader.java
+++ src/test/java/org/adorsys/waguia/lightxls/test/TestEasyXlsLoader.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2012 Waguia W. Boris boris.waguia@adorsys.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.adorsys.waguia.lightxls.test;
 
 import java.util.List;
@@ -9,7 +24,11 @@ import org.adorsys.waguia.lightxls.loader.EasyXlsLoader;
 import org.adorsys.waguia.lightxls.loader.Person;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.junit.Test;
-
+/**
+ * 
+ * @author w2b
+ *
+ */
 public class TestEasyXlsLoader {
 
 	@Test
